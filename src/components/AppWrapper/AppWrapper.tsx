a75f89094f0473da8b3835709da6a8284f4cc5dc
Filip Havrlent <filip@havrlent.com>
2022-07-06 18:55:17 +0200

    feat: add dark / light mode switcher

--- src/components/AppWrapper/AppWrapper.tsx
+++ src/components/AppWrapper/AppWrapper.tsx
@@ -1,9 +1,27 @@
 import { Suspense } from 'react';
-import { AppShell, Center, Container, Footer, Loader, useMantineTheme } from '@mantine/core';
+import {
+  ActionIcon,
+  AppShell,
+  Center,
+  Container,
+  Footer,
+  Group,
+  Header,
+  Loader,
+  Title,
+  Tooltip,
+  useMantineColorScheme,
+  useMantineTheme,
+} from '@mantine/core';
 import { Outlet } from 'react-router-dom';
+import { Sun, MoonStars } from 'tabler-icons-react';
 
 export const AppWrapper = () => {
   const theme = useMantineTheme();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+
+  const isDarkColorScheme = colorScheme === 'dark';
+
   return (
     <AppShell
       styles={{
@@ -23,6 +41,18 @@ export const AppWrapper = () => {
         <Footer height={60} p='md'>
           <Container>lorem ipsum</Container>
         </Footer>
+      }
+      header={
+        <Header height={60}>
+          <Group sx={{ height: '100%' }} px={20} position='apart'>
+            <Title>Secret Notes</Title>
+            <Tooltip label={isDarkColorScheme ? 'Light Mode' : 'Dark Mode'}>
+              <ActionIcon variant='default' onClick={() => toggleColorScheme()} size={30}>
+                {isDarkColorScheme ? <Sun size={16} /> : <MoonStars size={16} />}
+              </ActionIcon>
+            </Tooltip>
+          </Group>
+        </Header>
       }
     >
       <Container>
