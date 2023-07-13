87765cfdba92a2a33a77002730bada68e01d9043
Filip Havrlent <filip@havrlent.com>
2023-07-13 20:06:53 +0200

    feat: cleanup

--- src/components/AppWrapper/Footer.tsx
+++ src/components/AppWrapper/Footer.tsx
@@ -1,14 +1,12 @@
 import { Container, Footer as FooterComponent, Anchor } from '@mantine/core';
 import { BrandGithub } from 'tabler-icons-react';
 
-export const Footer = () => {
-  return (
-    <FooterComponent height={60} p='md'>
-      <Container>
-        <Anchor href='https://github.com/fhavrlent/secret-notes' target='_blank'>
-          <BrandGithub />
-        </Anchor>
-      </Container>
-    </FooterComponent>
-  );
-};
+export const Footer = () => (
+  <FooterComponent height={60} p='md'>
+    <Container>
+      <Anchor href='https://github.com/fhavrlent/secret-notes' target='_blank'>
+        <BrandGithub />
+      </Anchor>
+    </Container>
+  </FooterComponent>
+);
--- src/pages/ReadMessageNotice/useGetNote.ts
+++ src/pages/ReadMessageNotice/useGetNote.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
+
 import { API_URL } from '../../constants';
 
 export const useGetNote = (id?: string) =>
